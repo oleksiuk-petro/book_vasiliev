a7e24bc140f38f17b0e6695f894ec80bc839c047
oleksiuk-petro <oleksiuk.petro1993@gmail.com>
2021-10-20 23:20:36 +0300

    Передача аргументом поля об’єкту.

---
+++ 08_use_classes_objects/src/SwapFieldsApplication/SwapFieldsDemo.java
@@ -0,0 +1,31 @@
+package SwapFieldsApplication;
+
+class SwapFieldsDemo {
+
+    static void swap(MyClass A, MyClass B) {
+        System.out.println("Виконується метод swap()");
+        System.out.println("Значення полів до зміни значень:");
+        System.out.println("Об’єкт А: " + A.number);
+        System.out.println("Об’єкт В: " + B.number);
+
+        // обмін значеннями
+        int x = B.number;
+        B.number = A.number;
+        A.number = x;
+
+        System.out.println("Значення полів після зміни значень:");
+        System.out.println("Об’єкт А: " + A.number);
+        System.out.println("Об’єкт В: " + B.number);
+        System.out.println("Завершення методу swap()");
+    }
+
+    public static void main(String[] args) {
+        // створюємо об’єкти
+        MyClass objA = new MyClass(100);
+        MyClass objB = new MyClass(200);
+
+        System.out.println("Змінні до виклику методу swap(): " + objA.number + " та " + objB.number);
+        swap(objA, objB);
+        System.out.println("Змінні після виклику методу swap(): " + objA.number + " та " + objB.number);
+    }
+}
